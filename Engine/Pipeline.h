24b44225dc00d169e4c0e7ab7d254cbdefd2ddfb
MadMaia3D <33674747+MadMaia3D@users.noreply.github.com>
2024-01-07 18:15:52 -0300

    refactor: Pipeline triangle drawing logic to share common code

--- Engine/Pipeline.h
+++ Engine/Pipeline.h
@@ -24,7 +24,7 @@ public:
 			tc += rhs.tc;
 			return *this;
 		}
-		Vertex operator+(const Vertex& rhs) {
+		Vertex operator+(const Vertex& rhs) const {
 			return Vertex(*this) += rhs;
 		}
 		Vertex& operator-=(const Vertex& rhs) {
@@ -32,7 +32,7 @@ public:
 			tc -= rhs.tc;
 			return *this;
 		}
-		Vertex operator-(const Vertex& rhs) {
+		Vertex operator-(const Vertex& rhs) const {
 			return Vertex(*this) -= rhs;
 		}
 		Vertex& operator*=(float rhs) {
@@ -40,7 +40,7 @@ public:
 			tc *= rhs;
 			return *this;
 		}
-		Vertex operator*(float rhs) {
+		Vertex operator*(float rhs) const {
 			return Vertex(*this) *= rhs;
 		}
 		Vertex& operator/=(float rhs) {
@@ -48,7 +48,7 @@ public:
 			tc /= rhs;
 			return *this;
 		}
-		Vertex operator/(float rhs) {
+		Vertex operator/(float rhs) const {
 			return Vertex(*this) /= rhs;
 		}
 		Vec3 pos{};
@@ -103,10 +103,10 @@ private:
 	}
 
 	void RasterizeTriangle(Vertex& v0, Vertex& v1, Vertex& v2) {
-		DrawTriangle(v0, v1, v2, *texture);
+		DrawTriangle(v0, v1, v2);
 	}
 
-	void DrawTriangle(const Vertex & v0, const Vertex & v1, const Vertex & v2, const Surface& surf) {
+	void DrawTriangle(const Vertex & v0, const Vertex & v1, const Vertex & v2) {
 		const Vertex* pv0 = &v0;
 		const Vertex* pv1 = &v1;
 		const Vertex* pv2 = &v2;
@@ -118,10 +118,10 @@ private:
 		// for each case, points should be ordered in clockwise direction, starting from the most top-left point
 		if (pv0->pos.y == pv1->pos.y) {									// render flat top triangle
 			if (pv1->pos.x < pv0->pos.x) { std::swap(pv0, pv1); }
-			DrawFlatTopTriangle(*pv0, *pv1, *pv2, surf);
+			DrawFlatTopTriangle(*pv0, *pv1, *pv2);
 		} else if (pv1->pos.y == pv2->pos.y) {							// render flat bottom triangle
 			if (pv1->pos.x < pv2->pos.x) { std::swap(pv1, pv2); }
-			DrawFlatBottomTriangle(*pv0, *pv1, *pv2, surf);
+			DrawFlatBottomTriangle(*pv0, *pv1, *pv2);
 		} else {												// render separated triangles
 			// find alpha for linear interpolation (find the percentage of middle y between bottom y and top y)
 			const float alpha = (pv1->pos.y - pv0->pos.y) / (pv2->pos.y - pv0->pos.y);
@@ -129,121 +129,71 @@ private:
 			const Vertex sv = Interpolate(*pv0, *pv2, alpha);
 
 			if (sv.pos.x < pv1->pos.x) {
-				DrawFlatBottomTriangle(*pv0, *pv1, sv, surf);
-				DrawFlatTopTriangle(sv, *pv1, *pv2, surf);
+				DrawFlatBottomTriangle(*pv0, *pv1, sv);
+				DrawFlatTopTriangle(sv, *pv1, *pv2);
 			} else {
-				DrawFlatBottomTriangle(*pv0, sv, *pv1, surf);
-				DrawFlatTopTriangle(*pv1, sv, *pv2, surf);
+				DrawFlatBottomTriangle(*pv0, sv, *pv1);
+				DrawFlatTopTriangle(*pv1, sv, *pv2);
 			}
 		}
 	}
 
 	// mind the order of vertices: clockwise from the top
-	void DrawFlatBottomTriangle(const Vertex & v0, const Vertex & v1, const Vertex & v2, const Surface& surf) {
-		// use linear equation (y = mx +b) but the version of x in function of y (x = wy + p),
-		// where w is the run over the rise (inverse of m) and p is the x-intercept (the original b is the y-intercept)
-		// "y in function of x" would cause problems with vertical lines since 'm' in that case would result in division by zero
-		// so we use "x in function of x" version cause the lines that we won't need to calculate the bottom horizontal edge of the triangle
-
-		// run over rise for left and right sides
+	void DrawFlatBottomTriangle(const Vertex & v0, const Vertex & v1, const Vertex & v2) {
+		// calculate step of left and right edges
 		const float deltaY = (v2.pos.y - v0.pos.y);
-		const float lw = (v2.pos.x - v0.pos.x) / deltaY;
-		const float rw = (v1.pos.x - v0.pos.x) / deltaY;
+		Vertex leftEdgeStep = (v2 - v0) / deltaY;
+		Vertex rightEdgeStep = (v1 - v0) / deltaY;
+		DrawFlatTriangle(v0, v1, v2, leftEdgeStep, rightEdgeStep, v0, v0);
+	}
 
-		// following Microsoft DirectX10 rasterization "top-edge" rule
+	// mind the order of vertices: clockwise from the top left
+	void DrawFlatTopTriangle(const Vertex & v0, const Vertex & v1, const Vertex & v2) {
+		// calculate step of left and right edges
+		const float deltaY = (v2.pos.y - v0.pos.y);
+		Vertex leftEdgeStep = (v2 - v0) / deltaY;
+		Vertex rightEdgeStep = (v2 - v1) / deltaY;
+		DrawFlatTriangle(v0, v1, v2, leftEdgeStep, rightEdgeStep, v0, v1);
+	}
+
+	void DrawFlatTriangle(const Vertex & v0,
+						  const Vertex & v1,
+						  const Vertex & v2,
+						  const Vertex& leftEdgestep,
+						  const Vertex& rightEdgestep,
+						  Vertex leftEdgeStartVertex,
+						  Vertex rightEdgeStartVertex) {
+		// Microsoft DirectX10 rasterization "top-edge" rule
 		const int yStart = (int)std::ceil(v0.pos.y - 0.5f);
 		const int yEnd = (int)std::ceil(v2.pos.y - 0.5f);
 
-		// init text coord edges
-		Vec2 tcEdgeL = v0.tc;
-		Vec2 tcEdgeR = v0.tc;
-
-		// calculate tex coord edge unit steps
-		const Vec2 tcEdgeStepL = (v2.tc - v0.tc) / (v2.pos.y - v0.pos.y);
-		const Vec2 tcEdgeStepR = (v1.tc - v0.tc) / (v1.pos.y - v0.pos.y);
-
-		// do tex coord pre step
-		tcEdgeL += tcEdgeStepL * ((float)yStart - v0.pos.y + 0.5f);
-		tcEdgeR += tcEdgeStepR * ((float)yStart - v0.pos.y + 0.5f);
+		// do pre step
+		Vertex&leftEdgeInterpolant = leftEdgeStartVertex;
+		Vertex& rightEdgeInterpolant = rightEdgeStartVertex;
+		leftEdgeInterpolant += leftEdgestep * (float(yStart) - leftEdgeInterpolant.pos.y + 0.5f);
+		rightEdgeInterpolant += rightEdgestep * (float(yStart) - rightEdgeInterpolant.pos.y + 0.5f);
 
 		// get texture info
-		const int surfaceWidth = surf.GetWidth();
-		const int surfaceHeight = surf.GetHeight();
+		const int surfaceWidth = texture->GetWidth();
+		const int surfaceHeight = texture->GetHeight();
 		const int sufaceMaxWidth = surfaceWidth - 1;
 		const int surfaceMaxHeight = surfaceHeight - 1;
 
 		for (int y = yStart; y < yEnd; y++,
-			 tcEdgeL += tcEdgeStepL, tcEdgeR += tcEdgeStepR) {
-			// add 0.5 to y value because we are making calculations based on the pixel center
-			const float px0 = lw * (y + 0.5f - v0.pos.y) + v0.pos.x;
-			const float px1 = rw * (y + 0.5f - v0.pos.y) + v0.pos.x;
+			 leftEdgeInterpolant += leftEdgestep, rightEdgeInterpolant += rightEdgestep) {
 			// following Microsoft DirectX10 rasterization "left-edge" rule
-			const int xStart = (int)std::ceil(px0 - 0.5f);
-			const int xEnd = (int)std::ceil(px1 - 0.5f);
+			const int xStart = (int)std::ceil(leftEdgeInterpolant.pos.x - 0.5f);
+			const int xEnd = (int)std::ceil(rightEdgeInterpolant.pos.x - 0.5f);
 
-			// calculate text coord scanline unit step
-			const Vec2 tcScanStep = (tcEdgeR - tcEdgeL) / (px1 - px0);
-			Vec2 tcScanPos = tcEdgeL + tcScanStep * ((float)xStart - px0 + 0.5f);
+			Vertex scanPos = leftEdgeInterpolant;
+			// calculate scanline unit step
+			const float deltaX = rightEdgeInterpolant.pos.x - leftEdgeInterpolant.pos.x;
+			Vertex scanPosDelta = (rightEdgeInterpolant - leftEdgeInterpolant) / deltaX;
 
-			for (int x = xStart; x < xEnd; x++, tcScanPos += tcScanStep) {
-				const int surfaceX = std::min(int(tcScanPos.x * surfaceWidth), sufaceMaxWidth);
-				const int surfaceY = std::min(int(tcScanPos.y * surfaceHeight), surfaceMaxHeight);
-				gfx.PutPixel(x, y, surf.GetPixel(surfaceX, surfaceY));
-			}
-		}
-	}
-
-	// mind the order of vertices: clockwise from the top left
-	void DrawFlatTopTriangle(const Vertex & v0, const Vertex & v1, const Vertex & v2, const Surface& surf) {
-		// use linear equation (y = mx +b) but the version of x in function of y (x = wy + p),
-		// where w is the run over the rise (inverse of m) and p is the x-intercept (the original b is the y-intercept)
-		// "y in function of x" would cause problems with vertical lines since 'm' in that case would result in division by zero
-		// so we use "x in function of x" version cause the lines that we won't need to calculate the top horizontal edge of the triangle
-
-		// run over rise for left and right sides
-		const float deltaY = (v2.pos.y - v0.pos.y);
-		const float lw = (v2.pos.x - v0.pos.x) / deltaY;
-		const float rw = (v2.pos.x - v1.pos.x) / deltaY;
-
-		// following Microsoft DirectX10 rasterization "top-edge" rule
-		const int yStart = (int)std::ceil(v0.pos.y - 0.5f);
-		const int yEnd = (int)std::ceil(v2.pos.y - 0.5f);
-
-		// init text coord edges
-		Vec2 tcEdgeL = v0.tc;
-		Vec2 tcEdgeR = v1.tc;
-
-		// calculate tex coord edge unit steps
-		const Vec2 tcEdgeStepL = (v2.tc - v0.tc) / (v2.pos.y - v0.pos.y);
-		const Vec2 tcEdgeStepR = (v2.tc - v1.tc) / (v2.pos.y - v1.pos.y);
-
-		// do tex coord pre step
-		tcEdgeL += tcEdgeStepL * (float(yStart) - v0.pos.y + 0.5f);
-		tcEdgeR += tcEdgeStepR * (float(yStart) - v1.pos.y + 0.5f);
-
-		for (int y = yStart; y < yEnd; y++,
-			 tcEdgeL += tcEdgeStepL, tcEdgeR += tcEdgeStepR) {
-			// add 0.5 to y value because we are making calculations based on the pixel center
-			const float px0 = lw * (y + 0.5f - v0.pos.y) + v0.pos.x;
-			const float px1 = rw * (y + 0.5f - v1.pos.y) + v1.pos.x;
-			// following Microsoft DirectX10 rasterization "left-edge" rule
-			const int xStart = (int)std::ceil(px0 - 0.5f);
-			const int xEnd = (int)std::ceil(px1 - 0.5f);
-
-			// calculate text coord scanline unit step
-			const Vec2 tcScanStep = (tcEdgeR - tcEdgeL) / (px1 - px0);
-			Vec2 tcScanPos = tcEdgeL + tcScanStep * ((float)xStart - px0 + 0.5f);
-
-			// get texture info
-			const int surfaceWidth = surf.GetWidth();
-			const int surfaceHeight = surf.GetHeight();
-			const int sufaceMaxWidth = surfaceWidth - 1;
-			const int surfaceMaxHeight = surfaceHeight - 1;
-
-			for (int x = xStart; x < xEnd; x++, tcScanPos += tcScanStep) {
-				const int surfaceX = std::min(int(tcScanPos.x * surfaceWidth), sufaceMaxWidth);
-				const int surfaceY = std::min(int(tcScanPos.y * surfaceHeight), surfaceMaxHeight);
-				gfx.PutPixel(x, y, surf.GetPixel(surfaceX, surfaceY));
+			for (int x = xStart; x < xEnd; x++, scanPos += scanPosDelta) {
+				const int surfaceX = std::min(int(scanPos.tc.x * surfaceWidth), sufaceMaxWidth);
+				const int surfaceY = std::min(int(scanPos.tc.y * surfaceHeight), surfaceMaxHeight);
+				gfx.PutPixel(x, y, texture->GetPixel(surfaceX, surfaceY));
 			}
 		}
 	}
