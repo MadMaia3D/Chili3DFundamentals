51c656e6bbf8f9036d04f03452c7398575c03d0d
MadMaia3D <33674747+MadMaia3D@users.noreply.github.com>
2024-01-19 21:15:36 -0300

    change: cleanup Sphere header

--- Engine/Sphere.h
+++ Engine/Sphere.h
@@ -1,94 +1,9 @@
 #pragma once
-#include <vector>
-#include "Vector3.h"
-#include "ChiliMath.h"
-#include "IndexedLineList.h"
-#include "IndexedTriangleList.h"
-#include "Matrix3.h"
 
 class Sphere {
 public:
-	static IndexedLineList GetSphereOld(float radius = 0.5f, int segments = 16, int rings = 8){
-		const float latitude_angle_step = PI / rings;
-		const float longitude_angle_step = PI * 2.0f / segments;
-
-		const int total_vertices = segments * (rings - 1) + 2;
-		std::vector<Vec3> positions;
-		positions.reserve(total_vertices);
-
-		// top vertice
-		positions.emplace_back(0.0f, 0.0f, radius);
-
-		// middle vertice
-		for (size_t i = 1; i < rings; i++) {
-			const float latitude = i * latitude_angle_step + PI/2;
-			const float z = sin(latitude) * radius;
-			const float ring_radius = cos(latitude) * radius;
-
-			for (size_t i = 0; i < segments; i++) {
-				const float longitude = i * longitude_angle_step;
-				positions.emplace_back(cos(longitude) * ring_radius, sin(longitude) * ring_radius, z);
-			}
-		}
-		// bottom vertice
-		positions.emplace_back(0.0f, 0.0f, -radius);
-
-		std::vector<int> indices;
-
-		auto calculateIndex = [segments](int iLat, int iLong) {
-			return std::max(0, (iLat - 1) * segments + iLong);
-		};
-
-		// distribute faces
-		for (int iLat = 1; iLat < rings - 1; iLat++) {
-			for (int iLong = 1; iLong < segments; iLong++) {
-				indices.push_back(calculateIndex(iLat, iLong));
-				indices.push_back(calculateIndex(iLat + 1, iLong));
-				indices.push_back(calculateIndex(iLat + 1, iLong + 1));
-
-				indices.push_back(calculateIndex(iLat, iLong));
-				indices.push_back(calculateIndex(iLat + 1, iLong + 1));
-				indices.push_back(calculateIndex(iLat, iLong + 1));
-			}
-			indices.push_back(calculateIndex(iLat, segments));
-			indices.push_back(calculateIndex(iLat, 1));
-			indices.push_back(calculateIndex(iLat + 1, 1));
-
-			indices.push_back(calculateIndex(iLat, segments));
-			indices.push_back(calculateIndex(iLat + 1, 1));
-			indices.push_back(calculateIndex(iLat + 1, segments));
-		}
-
-		// sphere top indices
-		const int first_index = 0;
-		for (int i = 1; i < segments; i++) {
-			indices.push_back(first_index);
-			indices.push_back(i);
-			indices.push_back(i + 1);
-		}
-		indices.push_back(first_index);
-		indices.push_back(segments);
-		indices.push_back(1);
-
-		// sphere bottom indices
-		const int last_index = total_vertices - 1;
-		const int bottom_ring_first_index = segments * (rings - 2) + 1;
-		const int bottom_ring_last_index = last_index - 1;
-		for (int i = bottom_ring_first_index; i < bottom_ring_last_index; i++) {
-			indices.push_back(last_index);
-			indices.push_back(i);
-			indices.push_back(i + 1);
-		}
-		indices.push_back(last_index);
-		indices.push_back(bottom_ring_first_index);
-		indices.push_back(bottom_ring_last_index);
-
-		return { positions , indices};
-	}
-
-
 	template <typename Vertex>
-	static IndexedTriangleList<Vertex> GetSphere(float radius = 0.5f, int segments = 16, int rings = 8) {
+	static IndexedTriangleList<Vertex> GetPlainSphere(float radius = 0.5f, int segments = 16, int rings = 8) {
 		const float latitude_angle_step = PI / rings;
 		const float longitude_angle_step = PI * 2.0f / segments;
 
@@ -171,17 +86,3 @@ public:
 		return { std::move(vertices) , std::move(indices) };
 	}
 };
-
-
-
-/*for (size_t i = 0; i <= rings; i++) {
-			const float latitude = i * latitude_angle_step;
-			const float z = sin(latitude) * radius;
-			const float ring_radius = cos(latitude) * radius;
-			positions.emplace_back(ring_radius, 0.0f, z);
-		}
-
-		for (size_t i = 0; i < segments; i++) {
-			const float longitude = i * longitude_angle_step;
-			positions.emplace_back(cos(longitude) * radius, sin(longitude) * radius, 0.0f);
-		}*/
